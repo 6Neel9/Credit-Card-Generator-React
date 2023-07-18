d74387050e68f0d70b8c6f612607a78a8160e0af
6Neel9 <nilmandal300@gmail.com>
2023-07-18 22:20:32 +0530

    Updates

---
+++ precache-manifest.c7259ea8af08a6cc9335a86f6406b7d3.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "42626e8b06298144c080",
+    "url": "/Credit-Card-Generator-React/static/css/main.40ab164a.chunk.css"
+  },
+  {
+    "revision": "42626e8b06298144c080",
+    "url": "/Credit-Card-Generator-React/static/js/main.42626e8b.chunk.js"
+  },
+  {
+    "revision": "975f63afe5057efb9a20",
+    "url": "/Credit-Card-Generator-React/static/js/1.975f63af.chunk.js"
+  },
+  {
+    "revision": "a40fc0b28868594de4b0",
+    "url": "/Credit-Card-Generator-React/static/js/2.a40fc0b2.chunk.js"
+  },
+  {
+    "revision": "6a5a15a131d00e5eeda3",
+    "url": "/Credit-Card-Generator-React/static/js/runtime~main.6a5a15a1.js"
+  },
+  {
+    "revision": "33d6bbef0946f7071752c239b14d4a67",
+    "url": "/Credit-Card-Generator-React/static/media/bg-card-front.33d6bbef.png"
+  },
+  {
+    "revision": "1a41a92ca7b04ab1362f1b2f6bb2a83a",
+    "url": "/Credit-Card-Generator-React/static/media/bg-card-back.1a41a92c.png"
+  },
+  {
+    "revision": "3b8e5db1428efb79aa0006dfbef1d690",
+    "url": "/Credit-Card-Generator-React/static/media/bg-main-mobile.3b8e5db1.png"
+  },
+  {
+    "revision": "530df842c362d952de57b6d262361efc",
+    "url": "/Credit-Card-Generator-React/static/media/icon-complete.530df842.svg"
+  },
+  {
+    "revision": "c3ea18eecbb33ec48c9b728ee857c3dd",
+    "url": "/Credit-Card-Generator-React/index.html"
+  }
+];
